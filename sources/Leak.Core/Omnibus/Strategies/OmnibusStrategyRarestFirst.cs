edd6738e08b39c5459215ae2b9001e48c01f26a0
Adrian Macal <adma@amacal.pl>
2016-09-18 16:35:32 +0200

    Improve CPU performance of the rarest-first strategy.

--- sources/Leak.Core/Omnibus/Strategies/OmnibusStrategyRarestFirst.cs
+++ sources/Leak.Core/Omnibus/Strategies/OmnibusStrategyRarestFirst.cs
@@ -9,18 +9,22 @@ namespace Leak.Core.Omnibus.Strategies
     {
         public override IEnumerable<OmnibusBlock> Next(OmnibusContext context, PeerHash peer, int count)
         {
-            int left = Math.Min(count, count - context.Reservations.Count(peer));
+            int current = context.Reservations.Count(peer);
+            int left = Math.Min(count, count - current);
 
-            if (left > 0)
+            if (current < 16 && left > 0)
             {
                 DateTime now = DateTime.Now;
                 Bitfield bitfield = context.Bitfields.ByPeer(peer);
 
                 int blocks = context.Metainfo.GetBlocksInPiece();
-                int pieces = context.Metainfo.Properties.Pieces;
+                int total = context.Metainfo.Properties.Pieces;
 
                 OmnibusBitfieldRanking ranking = context.Bitfields.Ranking;
                 OmnibusBitfieldRanking adjusted = ranking.Exclude(context.Pieces).Include(bitfield);
+
+                OmnibusPieceCollection pieces = context.Pieces;
+                OmnibusReservationCollection reservations = context.Reservations;
 
                 foreach (Bitfield best in adjusted.Order())
                 {
@@ -28,13 +32,13 @@ namespace Leak.Core.Omnibus.Strategies
                     long totalSize = context.Metainfo.Properties.TotalSize;
                     int blockSize = context.Metainfo.Properties.BlockSize;
 
-                    for (int i = 0; left > 0 && i < pieces; i++)
+                    for (int i = 0; left > 0 && i < total; i++)
                     {
                         if (positive > 0 && best[i])
                         {
                             for (int j = 0; left > 0 && totalSize > 0 && j < blocks; j++)
                             {
-                                if (context.Pieces.IsComplete(i, j) == false)
+                                if (pieces.IsComplete(i, j) == false)
                                 {
                                     int offset = j * blockSize;
                                     int nextSize = blockSize;
@@ -45,8 +49,8 @@ namespace Leak.Core.Omnibus.Strategies
                                     }
 
                                     OmnibusBlock block = new OmnibusBlock(i, offset, nextSize);
-                                    bool contains = context.Reservations.Contains(block, now) ||
-                                                    context.Reservations.Contains(block, peer);
+                                    bool contains = reservations.Contains(block, now) ||
+                                                    reservations.Contains(block, peer);
 
                                     if (contains == false)
                                     {
@@ -58,7 +62,7 @@ namespace Leak.Core.Omnibus.Strategies
                                 totalSize = totalSize - blockSize;
                             }
 
-                            positive = positive + 1;
+                            positive = positive - 1;
                         }
                         else
                         {
